7c048f03430b25e53c0b0515b021403c0dcd426a
EDUARDOTORRES16 <161844927+EDUARDOTORRES16@users.noreply.github.com>
2024-12-31 02:24:24 +0100

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import PyPDF2
 import streamlit as st
 
-# Cambiar la fuente personalizada
+# Cambiar la fuente personalizada y estilos
 st.markdown(
     """
     <style>
@@ -10,36 +10,38 @@ st.markdown(
     html, body, [class*="css"]  {
         font-family: 'Roboto', sans-serif;
     }
+    h1 {
+        color: #FE7235;
+    }
     </style>
     """,
     unsafe_allow_html=True
 )
 
 # Añadir el logo
-st.image("https://upload.wikimedia.org/wikipedia/commons/a/ab/Logo_TV_2015.png", width=150)
+st.image("https://info.criteria.es/hubfs/Criteria_Logo_RGB.png", width=200)
 
 # Encabezado personalizado
 st.markdown(
     """
-    <h1 style="text-align: center; color: #4CAF50;">Extractor de Diplomas</h1>
+    <h1 style="text-align: center;">Extractor de Diplomas</h1>
     <p style="text-align: center;">Sube un archivo PDF y extrae los diplomas de los participantes ingresando sus DNIs separados por espacios.</p>
     """,
     unsafe_allow_html=True
 )
 
-# Inicializar variables de estado
-if "pdf_file" not in st.session_state:
-    st.session_state.pdf_file = None
+# Inicializar variables de estado para manejar los DNIs
 if "dni_input" not in st.session_state:
     st.session_state.dni_input = ""
 
-# Botón para reiniciar
+# Botón para reiniciar el cuadro de texto
 if st.button("Nueva Extracción"):
-    st.session_state.pdf_file = None
     st.session_state.dni_input = ""
 
-# Subida de PDF y entrada de DNIs
-pdf_file = st.file_uploader("Cargar el archivo PDF", type=["pdf"], key="pdf_file")
+# Subida de PDF (no requiere manejo de estado)
+pdf_file = st.file_uploader("Cargar el archivo PDF", type=["pdf"])
+
+# Entrada de DNIs
 dni_input = st.text_area("Introduce los DNIs separados por espacios:", value=st.session_state.dni_input, key="dni_input")
 
 # Botón para realizar la extracción
